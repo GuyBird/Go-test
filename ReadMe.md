9b6e695173440a87d391d417e44bb7638a9cb5d1
Guy Bird <06gbird@gmail.com>
2018-07-26 09:34:15 +0100

    First Commit

---
+++ ReadMe.md
@@ -0,0 +1,2 @@
+25/07/18
+Small hello world in golang to test go + docker + git all together
---
+++ hello_world.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func helloWorldTalker() string{
+	return "hello world"
+}
+
+func main() {
+	fmt.Println(helloWorldTalker())
+}
